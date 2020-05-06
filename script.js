8077871430704d13c2862612ab1b39ae41b6c629
Muki85 <mustafa.elaasad@gmail.com>
2020-05-06 09:56:53 +0200

    This is a new project to learn JS

---
+++ script.js
@@ -0,0 +1,19 @@
+// Creat const
+
+
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+
+
+// Run a standard minute/second/hundredths timer:
+
+
+// Match the text entered with the provided text on the page:
+
+
+// Start the timer:
+
+
+// Reset everything:
+
+
+// Event listeners for keyboard input and the reset button:
